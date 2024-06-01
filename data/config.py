6619ef720207041a762bfee0e2f0b72198b96462
VernonDev <91039933+VernonDevID@users.noreply.github.com>
2024-06-01 21:14:58 +0700

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,6 +1,6 @@
 # api id, hash
-API_ID = 1488
-API_HASH = '1488ugiukl'
+API_ID = 24339884
+API_HASH = '9ad37509f5764011937dcbbfb7b52f6f'
 
 DELAYS = {
     'ACCOUNT': [5, 15],  # delay between connections to accounts (the more accounts, the longer the delay)
